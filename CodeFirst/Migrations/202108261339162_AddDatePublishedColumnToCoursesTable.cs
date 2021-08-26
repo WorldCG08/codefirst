43283777a918e0ddbe3cf0a68fd78dd46a6cf1c5
ymelnyk <yurii.melnyk@dpgmm.net>
2021-08-26 16:43:46 +0300

    Added nullable datetime column to courses

---
+++ CodeFirst/Migrations/202108261339162_AddDatePublishedColumnToCoursesTable.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDatePublishedColumnToCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "DatePublished", c => c.DateTime());
+            
+            Sql("UPDATE Courses SET Description = 'Updated description from migration'");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Courses", "DatePublished");
+        }
+    }
+}
--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Diagnostics.Tracing;
 
@@ -17,6 +18,7 @@ namespace CodeFirst
         public string Title { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
+        public DateTime? DatePublished { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
         public IList<Tag> Tags { get; set; }
