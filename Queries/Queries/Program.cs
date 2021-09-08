f4c230205414687f57c4de4183e587ae607d27da
ymelnyk <yurii.melnyk@dpgmm.net>
2021-09-08 17:01:33 +0300

    some querying examples with LINQ

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -7,29 +7,63 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            // var context = new PlutoContext();
+            //
+            // // LINQ syntax
+            // var query =
+            //     from c in context.Courses
+            //     where c.Name.Contains("c#")
+            //     orderby c.Name
+            //     select c;
+            //
+            // foreach (var course in query)
+            // {
+            //     //Console.WriteLine(course.Name);
+            // }
+            //
+            // // Extension methods
+            // var courses = context.Courses
+            //     .Where(c => c.Name.Contains("c#"))
+            //     .OrderBy(c => c.Name);
+            //
+            // foreach (var course in courses)
+            // {
+            //     Console.WriteLine(course.Name);
+            // }
+            
+            
             var context = new PlutoContext();
             
-            // LINQ syntax
-            var query =
+            var restriction =
                 from c in context.Courses
-                where c.Name.Contains("c#")
-                orderby c.Name
+                where c.Level == 1 && c.Author.Id == 1
                 select c;
-
-            foreach (var course in query)
-            {
-                //Console.WriteLine(course.Name);
-            }
             
-            // Extension methods
-            var courses = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .OrderBy(c => c.Name);
+            var ordering =
+                from c in context.Courses
+                where c.Author.Id == 1
+                orderby c.Level descending, c.Name
+                select c;
+            
+            var ordering2 =
+                from c in context.Courses
+                where c.Author.Id == 1
+                orderby c.Level descending, c.Name
+                select c;
+            
+            var projection =
+                from c in context.Courses
+                where c.Author.Id == 1
+                orderby c.Level descending, c.Name
+                select new { Namme = c.Name, Author = c.Author.Name };
 
-            foreach (var course in courses)
-            {
-                Console.WriteLine(course.Name);
-            }
+            var grouping =
+                from c in context.Courses
+                group c by c.Level
+                into g
+                select g;
+
+
         }
     }
 }
